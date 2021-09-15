61a0a35176d62a28d3bd9e44777222bd6e00b79c
jaeyeon kim <kpeel5839@github.com>
2021-09-16 00:05:48 +0900

    restart inflearn

---
+++ hello-practice/src/main/java/com/example/hellopractice/HelloPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellopractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloPracticeApplication.class, args);
+	}
+
+}
---
+++ hello-practice/src/test/java/com/example/hellopractice/HelloPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
