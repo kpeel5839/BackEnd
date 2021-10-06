b536b20e4972439e8927c4e638e9e4f23ddf626b
jaeyeon kim <kpeel5839@github.com>
2021-10-06 14:04:41 +0900

    add comment

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
-    @Bean
+    @Bean //Bean is connect on server
     public MemberService memberService(){
         return new MemberService(memberRepository());
     }
